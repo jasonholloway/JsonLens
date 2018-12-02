5e3540549cfca5ee30f9507b28b90ba957acebb3
Jason Holloway <miceeatnicerice@hotmail.com>
2018-12-02 15:11:14 +0000

    settled on tokenizer scheme; notes added

---
+++ JsonLens.Test/JsonTokenizer.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JsonLens.Test
+{
+    using Result = ValueTuple<Status, int, Mode?>;
+
+
+    public class JsonTokenizer
+    {
+        public Result Tokenize(ref Context x)
+        {
+            if(x.Span.Length == 0) { //should just try reading, surely...
+                return Underrun;
+            }
+
+            if(x.Span[0] == 0) {    //but file-end makes no sense in the middle of most yieldings...
+                x.Emit(Token.End);  //it only makes sense at the end of a line
+                return End;
+            }
+
+            switch(x.Mode)
+            {
+                case Mode.Line:
+                    x.Emit(Token.NewLine);
+                    x.Push(Mode.LineEnd);
+                    return Ok(Mode.Value);
+
+                case Mode.LineEnd:
+                    throw new NotImplementedException("HELLO!!!!!");
+
+
+                case Mode.Value:
+                    switch(x.Span[0]) {
+                        case '"':
+                            x.Emit(Token.String);
+                            return Ok(1, Mode.String);
+
+                        case '{':
+                            x.Emit(Token.Object);
+                            return Ok(1, Mode.Object);
+
+                        case char c when c > 0:
+                            return ReadNumber(ref x);
+                    }
+                    break;
+
+                case Mode.Object:
+                    switch(x.Span[0]) {
+                        case '"':
+                            x.Emit(Token.Prop);
+                            return Ok(Mode.Prop);
+
+                        case '}':
+                            x.Emit(Token.ObjectEnd);
+                            return Ok(1, x.Pop());
+                    }
+                    break;
+                
+                case Mode.Prop:
+                    return Ok(Mode.String);
+
+                case Mode.ObjectValue:
+                    return Ok(Mode.Value);
+
+                case Mode.Array:
+                    break;
+
+                case Mode.String:
+                    return ReadString(ref x);
+            }
+
+            return BadInput;
+        }
+
+
+        Result ReadNumber(ref Context x)
+        {
+            for(int i = 1; i < x.Span.Length; i++)
+            {
+                if (!IsNumeric(x.Span[i]))
+                {
+                    x.Emit(Token.Number);
+                    return Ok(i, x.Pop());
+                }
+            }
+
+            return Underrun;
+        }
+
+        Result ReadString(ref Context x)
+        {
+            int i = 0;
+
+            for (i = 1; i < x.Span.Length; i++)
+            {
+                switch (x.Span[i])
+                {
+                    case '\\':      //BUT! what about "\\", eh???
+                        continue;
+
+                    case '"':
+                        x.Emit(Token.StringChunk);
+                        return Ok(i, x.Pop());
+                }
+            }
+
+            x.Emit(Token.StringChunk);  //but only if i > 1...!
+            return Underrun;
+        }
+
+        
+        bool IsNumeric(char c)
+            => c >= 48 && c < 58;
+
+
+
+        Result Ok(Mode next)
+            => (Status.Ok, 0, next);
+
+        Result Ok(int charsRead, Mode next)
+            => (Status.Ok, charsRead, next);
+
+        Result Underrun
+            => (Status.Underrun, 0, null);
+
+        Result End
+            => (Status.End, 0, null);
+
+        Result BadInput
+            => (Status.BadInput, 0, null);
+    }
+
+
+    public ref struct Context
+    {
+        public ReadOnlySpan<char> Span;
+        public int Index;
+        public Stack<Mode> ModeStack;
+        public List<(Token, (int, int)?)> Output;
+        public Mode Mode;
+
+        public Context(ReadOnlySpan<char> span, int index, Mode mode)
+        {
+            Span = span;
+            Index = index;
+            ModeStack = new Stack<Mode>();
+            Output = new List<(Token, (int, int)?)>();
+            Mode = mode;
+        }
+
+        public Status? Check(char c)
+        {
+            return null;
+        }
+
+        public Status? CheckForUnderrun()
+            => null;
+
+        public Status? NextIsNumeric()
+        {
+            return null;
+        }
+
+        public void Skip(int count)
+        {
+            Index += count;
+        }
+
+        public void Push(Mode mode)
+            => ModeStack.Push(mode);
+
+        public Mode Pop()
+            => ModeStack.Pop();
+
+
+        public void Emit(Token token)
+            => Output.Add((token, null));
+
+        public void Emit(Token token, int from, int to)
+            => Output.Add((token, (from, to)));
+
+    }
+
+
+    public enum Status : byte
+    {
+        Ok,
+        Underrun,
+        End,
+        BadInput
+    }
+
+    public enum Token : byte
+    {
+        None,
+        End,
+        Empty,
+        NewLine,
+        Comment,
+        Object,
+        ObjectEnd,
+        Colon,
+        String,
+        StringChunk,
+        StringEnd,
+        Number,
+        Prop,
+        Key
+    }
+
+    public enum Mode : byte
+    {
+        None,
+        Line,
+        Object,
+        Array,
+        Value,
+        String,
+        Prop,
+        ObjectValue,
+        Number,
+        LineEnd
+    }
+
+    public enum ModeAction : byte
+    {
+        None,
+        Switch,
+        Push,
+        Pop
+    }
+
+
+
+
+    //can't just put together different sub-parsings here
+    //we have to have different modes then
+    //which feels - complicated
+
+    //but if we're going to have everything inline, then we can't nicely yield...
+    //so we need different modes
+    //KeyValue mode
+    //String mode
+
+    //
+    //line
+    //  object
+    //    key
+    //      string
+    //        chunk
+    //    value
+    //      number
+
+    //but we can't 'orchestrate' two modes two follow one another
+    //as we have to plop back to the same context
+    //we wanna set the mode separately from the stack
+    //so... when we've parsed a key, then the handler will set the mode to 'value'
+    //and after 'value', we want to pop back to where we were previously
+    //so like a little subsystem of states
+
+    //the advantage in all this is the yielding which allows us to resume midstream
+
+    //otherwise we could just compose expectations within functions
+    //but then the AOP-style concerns have to be managed within app code
+    //at the call places, dispersed
+    //or - as we know - we'd need a more fancy way of gluing computations together
+
+    //but we don't have that... not easily, and not without lambdas and suchlike.
+    //if we wanted that kind of niceness, it'd be most snazzy to have some interpretable structure
+    //which would allow us to piece shit together
+    //static structures that can be piced togther...
+
+    //and so we have another potential output from the handler
+    //PushMode
+    //PopMode
+    //SetMode
+    //None
+
+    //PushMode would set the mode and push the mode
+    //so at the point of entering the mode 
+    //we will be expected to know the semantics of return
+    //an object isn't just a transient mode, but a lingering context
+
+    //so when we find an object,
+    //we need to say we're now in object mode
+    //though really now we're in key mode
+    //and then we'll be in value mode
+    //and then key mode or popped back out
+
+    //when we are in value mode (and only value mode?)
+    //then we need to push to the stack
+    //otherwise there's a clear 
+    //so with every granule, we can emit a token, switch a mode, push a mode, or pop a mode
+    //and we also want to state the number of characters read
+    //and whether we've had an underrun or not
+
+    //---------------------------------------------------
+
+    //so we're recreating the stack, and a kind of dispatch, instead of just using the facilities of the language
+    //and that's all so we can yield
+    //
+    //
+    //
+
+
+}
--- JsonLens.Test/TokenizerTests.cs
+++ JsonLens.Test/TokenizerTests.cs
@@ -1,48 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using Xunit;
 using Shouldly;
-using System.Runtime.InteropServices;
 using System.Linq;
 
 namespace JsonLens.Test
 {
     public class TokenizerTests
     {
-        List<(int, Token)> Results;
         JsonTokenizer Tokenizer;
 
         public TokenizerTests()
         {
-            Results = new List<(int, Token)>();
             Tokenizer = new JsonTokenizer();
-        }
-
-        [Fact]
-        public void OpeningBrace()
-        {
-            var result = Tokenizer.Read("{}".AsSpan()).Value;
-            result.CharsRead.ShouldBe(1);
-            result.Token.ShouldBe(Token.Object);
-        }
-
-        [Fact]
-        public void OpenClosedBraces()
-        {
-            int i = 0;
-            var span = "{}".AsZeroTerminatedSpan();
-
-            var result1 = Tokenizer.Read(span.Slice(i)).Value;
-            result1.CharsRead.ShouldBe(1);
-            result1.Token.ShouldBe(Token.Object);
-
-            var result2 = Tokenizer.Read(span.Slice(i += result1.CharsRead)).Value;
-            result2.CharsRead.ShouldBe(1);
-            result2.Token.ShouldBe(Token.EndObject);
-
-            var result3 = Tokenizer.Read(span.Slice(i += result2.CharsRead)).Value;
-            result3.CharsRead.ShouldBe(1);
-            result3.Token.ShouldBe(Token.End);
         }
 
         public class WithSimpleDriver
@@ -59,7 +28,7 @@ namespace JsonLens.Test
                 => Tokenize("{}")
                     .ShouldBe(new[] {
                         (Token.Object, "{"),
-                        (Token.EndObject, "}"),
+                        (Token.ObjectEnd, "}"),
                         (Token.End, "")
                     });
 
@@ -67,7 +36,7 @@ namespace JsonLens.Test
             public void SimpleString()
                 => Tokenize("\"Hello!!!\"")
                     .ShouldBe(new[] {
-                        (Token.String, "\"Hello!!!\""),
+                        (Token.String, "Hello!!!"),
                         (Token.End, "")
                     });
 
@@ -75,17 +44,19 @@ namespace JsonLens.Test
             public void SimpleString_WithEscapedQuote()
                 => Tokenize("\"Bl\\\"ah\"")
                     .ShouldBe(new[] {
-                        (Token.String, "\"Bl\\\"ah\""),
+                        (Token.String, "Bl\\\"ah"),
                         (Token.End, "")
                     });
 
             [Fact]
-            public void StringColonString()
-                => Tokenize("\"wibble\":\"blah\"")
+            public void ObjectWithProperty()
+                => Tokenize("{\"wibble\":\"blah\"}")
                     .ShouldBe(new[] {
-                        (Token.String, "\"wibble\""),
-                        (Token.Colon, ":"),
-                        (Token.String, "\"blah\""),
+                        (Token.Object, "{"),
+                        (Token.Prop, ""),
+                        (Token.String, "wibble"),
+                        (Token.String, "blah"),
+                        (Token.ObjectEnd, "}"),
                         (Token.End, "")
                     });
 
@@ -101,244 +72,75 @@ namespace JsonLens.Test
         static (Token, string)[] Tokenize(string input)
         {
             var tokenizer = new JsonTokenizer();
-
-            var span = input.AsZeroTerminatedSpan();
-
-            var results = new List<(Token, string)>();
-            bool go = true;
-            int i = 0;
-
-            while (go)
+            var x = new Context(input.AsZeroTerminatedSpan(), 0, Mode.Line);
+            
+            while (true)
             {
-                var result = tokenizer.Read(span.Slice(i));
-                if (!result.HasValue) break; //should throw error???
-
-                switch (result.Value.Type)
+                var (status, charsRead, nextMode) = tokenizer.Tokenize(ref x);       
+                
+                switch(status)
                 {
-                    case ResultType.Underrun:
-                        go = false;
+                    case Status.Ok:
+                        x.Span = x.Span.Slice(charsRead);
+                        x.Mode = nextMode.Value;
                         break;
 
-                    case ResultType.Token:
-                        var token = result.Value.Token.Value;
-                        var charsRead = result.Value.CharsRead;
-                        results.Add((token, new string(span.Slice(i, charsRead).ToArray())));
-                        i += charsRead;
-                        break;
+                    case Status.End:
+                        return x.Output
+                                .Select(o => (o.Item1, ""))
+                                .ToArray();
+
+                    case Status.Underrun:
+                        throw new NotImplementedException("UNDERRUN to do!");
+
+                    case Status.BadInput:
+                        throw new NotImplementedException("BADINPUT to do!");
                 }
             }
 
-            return results.ToArray();
         }
     }
     
-    public class JsonTokenizer
+
+    public class ContextTests
     {
-        Stack<Mode> _modes;
+        //[Fact]
+        //public void StateMutates()
+        //{
+        //    var context = new Context("hello".AsSpan());
+        //    Blah(ref context);
+        //    Blah(ref context);
+        //    context.Index.ShouldBe(4);
 
-        public Result? Parse(ReadOnlySpan<char> inp)
+        //    void Blah(ref Context x)
+        //        => x.Emit(Token.Colon, 2);
+        //}
+
+        [Fact]
+        public void NullFallback_WorksAsExpected()
         {
-            switch(_modes.Peek())
-            {
-                case Mode.Line:
-                    return ParseString(inp)
-                        ?? ParseObject(inp);
+            var r = Ok() ?? OhDear();
+            r.ShouldBe(Status.Ok);
+            
+            Status? Ok()
+                => Status.Ok;
 
-                case Mode.Object:
-                    return ParseKeyValue(inp)
-                        ?? ParseObjectEnd(inp);
-
-                case Mode.Array:
-                    break;
-            }
-            throw new NotImplementedException();
+            Status? OhDear()
+                => throw new Exception("bugger");
         }
 
-        Result? ParseObject(ReadOnlySpan<char> inp)
+        [Fact]
+        public void BoolFallback_SurprisinglyAlsoWorks()
         {
-            if(inp[0] == '{')
-            {
-                _modes.Push(Mode.Object);
-                return (Token.Object, 1);
-            }
+            var r = Ok() || OhDear();
+            r.ShouldBe(true);
 
-            return Skip();
+            bool Ok()
+                => true;
+
+            bool OhDear()
+                => throw new Exception("bugger");
         }
-
-        Result? ParseObjectEnd(ReadOnlySpan<char> inp)
-        {
-            if(inp[0] == '}')
-            {
-                _modes.Pop();
-                return (Token.EndObject, 1);
-            }
-
-            return Skip();
-        }
-
-        Result? ParseKeyValue(ReadOnlySpan<char> inp)
-        {
-            throw new NotImplementedException();
-        }
-        
-        Result? ParseString(ReadOnlySpan<char> inp)
-        {
-            if(inp[0] == '"')
-            {
-                for (int i = 1; i < inp.Length; i++)
-                {
-                    switch (inp[i])
-                    {
-                        case '\\':
-                            i++;
-                            break;
-
-                        case '"':
-                            return (Token.String, i + 1);
-                    }
-                }
-
-                return Underrun();
-            }
-
-            return Skip();
-        }
-
-
-        public Result? Read(ReadOnlySpan<char> inp)
-        {
-            if (inp.Length == 0)
-                return Underrun();
-            else 
-                return ReadEnd(inp)
-                    ?? ReadSyntax(inp)
-                    ?? ReadString(inp)
-                    ?? ReadNumber(inp);
-        }
-
-        Result? ReadEnd(ReadOnlySpan<char> inp)
-            => inp[0] == 0
-                ? (Token.End, 1)
-                : Skip();
-        
-        Result? ReadSyntax(ReadOnlySpan<char> inp)
-        {
-            switch(inp[0])
-            {
-                case '{':
-                    return (Token.Object, 1);
-
-                case '}':
-                    return (Token.EndObject, 1);
-
-                case ':':
-                    return (Token.Colon, 1);
-
-                default:
-                    return Skip();
-            }
-        }
-
-        Result? ReadString(ReadOnlySpan<char> inp)
-        {
-            if (inp[0] != '"') return Skip();
-
-            for (int i = 1; i < inp.Length; i++)
-            {
-                switch (inp[i])
-                {
-                    case '\\':
-                        i++;
-                        break;
-
-                    case '"':
-                        return (Token.String, i + 1);
-                }
-            }
-
-            return Underrun();
-        }
-
-        Result? ReadNumber(ReadOnlySpan<char> inp)
-        {
-            if (!IsNumeric(inp[0])) return Skip();
-
-            for(int i = 0; i < inp.Length; i++) {
-                if (!IsNumeric(inp[i])) {
-                    return (Token.Number, i);
-                }
-            }
-
-            return Underrun();
-
-            //how can we distinguish between a real end
-            //and a simple underrun? Some kind of special character from above - a '0' maybe
-            //otherwise we just need more input to decide...
-            //and if we can't decide, summat should go 'pop'
-
-            //so we'd have a 'ReadEnd' strat 
-            //
-            //
-            //
-        }
-
-        bool IsNumeric(char c)
-            => c >= 48 && c < 58;
-
-
-        Result? Skip()
-            => null;
-
-        Result? Underrun()
-            => ResultType.Underrun;
-
-
-        public struct Result
-        {
-            public readonly ResultType Type;
-            public readonly Token? Token;
-            public readonly int CharsRead;
-
-            public Result(ResultType type, Token? token = null, int charsRead = 0)
-            {
-                Type = type;
-                Token = token;
-                CharsRead = charsRead;
-            }
-
-            public static implicit operator Result((Token, int) token)
-                => new Result(ResultType.Token, token.Item1, token.Item2);
-
-            public static implicit operator Result(ResultType result)
-                => new Result(result);
-        }
-    }
-
-    public enum ResultType : byte
-    {
-        Underrun,
-        Token
-    }
-
-    public enum Token : byte
-    {
-        End,
-        Empty,
-        NewLine,
-        Comment,
-        Object,
-        EndObject,
-        Colon,
-        String,
-        Number
-    }
-    
-    public enum Mode : byte
-    {
-        Line,
-        Object,
-        Array,
-        Value
     }
 
 }
